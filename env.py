839f0e1c14ddbadc06277a695ce7e5dab8a5a2f3
John Ennis <johnennis5@gmail.com>
2019-07-01 19:43:35 +0100

    Revert "Added images"
    This reverts commit 665d3dfa

--- env.py
+++
@@ -1,4 +0,0 @@
-import os
-
-os.environ.setdefault('SECRET_KEY', 'gl-il@^y3avg)y&7yokx#_k#fge^j121x9qfs(m^-(#xr_yncs')
-os.environ.setdefault('DATABASE_URL', 'postgres://znlcybdwuttgiq:8c44eef67deee70fc6bc2cd3e829c49db94775accbf00ba4ba53d11f04a218d3@ec2-54-243-47-196.compute-1.amazonaws.com:5432/d6q4r8nqjl3p26')
